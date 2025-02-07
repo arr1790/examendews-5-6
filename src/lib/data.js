3be2d159570f9e2541ff0bdf17a71602023647d3
arr1790 <anaruano1790@gmail.com>
2025-02-07 18:26:01 +0100

    ejercicio2

---
+++ src/lib/data.js
@@ -0,0 +1,57 @@
+'use server'
+
+import prisma from "@/lib/prisma"
+
+
+//---------Repartidores---------------------
+export async function obtenerRepartidores() {
+    const repartidores = await prisma.repartidor.findMany();
+    return repartidores;
+}
+
+export async function obtenerRepartidor(id) {
+    const repartidor = await prisma.repartidor.findUnique({
+        where: { id: +id }
+    });
+    return repartidor;
+}
+
+
+
+//---------Pedidos---------------------
+
+export async function obtenerPedidos() {
+    const pedidos = await prisma.pedido.findMany();
+    return pedidos;
+}
+
+
+   
+
+ export async function obtenerPedido (id) {
+    const pedido = await prisma.pedido.findUnique({
+        where: { id: +id },
+        include: {
+             pizzas: true,
+              repartidor: true
+        }    
+    })
+    return pedido
+}
+
+
+
+
+
+//---------Pizzas---------------------
+export async function obtenerPizzas() {
+    const pizzas = await prisma.pizza.findMany();
+    return pizzas;
+}
+
+export async function obtenerPizza(id) {
+    const pizza = await prisma.pizza.findUnique({
+        where: { id: +id }
+    });
+    return pizza;
+}
